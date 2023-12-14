2fd121de1b6d28dde2d5c79bd3518f952408b7b0
kdivanovich <kndivanovich@gmail.com>
2023-12-14 22:18:05 +0200

    Switching the active player.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0Elem = document.querySelector('#score--0');
 const score1Elem = document.getElementById('score--1');
 const curr0Elem = document.querySelector('#current--0');
@@ -15,7 +18,9 @@ score0Elem.textContent = 0;
 score1Elem.textContent = 0;
 diceElem.classList.add('hidden');
 
-let currScore = 0;
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 
 // Rolling the die funcionality
@@ -28,12 +33,21 @@ btnRoll.addEventListener('click', function() {
     diceElem.classList.remove('hidden');
     diceElem.src = `dice-${die}.png`;
 
-    // 3. Chck for rolled 1: if yes -> switch player 
+    // 3. Check for rolled 1: if 
+    // no -> accumulate score, display it;   yes -> switch player 
     if (die != 1) {
-        // add die to curr score
-        currScore += die;
-        curr0Elem.textContent = currScore;  // change for curr player 
+        // Add die to curr score
+        currentScore += die;
+        // Select the score dynamically based on which the current player is: 
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
+        // Player switch
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        currentScore = 0; 
+
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
 
     }
 
